3c333922b79f91200d2af36bdb5542d87427930c
brando1607 <109045330+brando1607@users.noreply.github.com>
2025-03-23 18:56:05 -0300

    chore(books): routes and methods created for books in  api gateway service

--- api-gateway/src/books/book.controller.ts
+++ api-gateway/src/books/book.controller.ts
@@ -8,8 +8,67 @@ import {
   Body,
 } from '@nestjs/common';
 import { BookService } from './book.service';
+import { Book, PartialBook } from './types';
 
 @Controller('book')
 export class BookController {
   constructor(private readonly bookService: BookService) {}
+
+  @Get()
+  async getBooks(): Promise<Book[] | string> {
+    try {
+      const result = await this.bookService.getBooks();
+
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Get(':id')
+  async getBook(@Param('id') id: string): Promise<Book | string> {
+    try {
+      const result = await this.bookService.getBook(id);
+
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Post()
+  async createBook(@Body() newBook: Book): Promise<Book | string> {
+    try {
+      const result = await this.bookService.createBook(newBook);
+
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Put(':id')
+  async updateBook(
+    @Body() newData: PartialBook,
+    @Param('id') id: string,
+  ): Promise<Book | string> {
+    try {
+      const result = await this.bookService.updateBook(id, newData);
+
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  @Delete('id')
+  async deleteBook(@Param('id') id: string): Promise<Book | string> {
+    try {
+      const result = await this.bookService.deleteBook(id);
+
+      return result;
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 }
--- api-gateway/src/books/book.service.ts
+++ api-gateway/src/books/book.service.ts
@@ -1,7 +1,69 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
+import { Book, PartialBook } from './types';
 
 @Injectable()
 export class BookService {
   constructor(@Inject('BOOK_SERVICE') private client: ClientProxy) {}
+
+  async getBooks(): Promise<Book[] | string> {
+    try {
+      const result = this.client.send({ cmd: 'getBooks' }, {});
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getBook(id: string): Promise<Book | string> {
+    try {
+      const result = this.client.send({ cmd: 'geBook' }, id);
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async createBook(newBook: Book): Promise<Book | string> {
+    try {
+      const result = this.client.send({ cmd: 'createBook' }, newBook);
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async updateBook(id: string, newData: PartialBook): Promise<Book | string> {
+    try {
+      const result = this.client.send({ cmd: 'updateBook' }, { id, newData });
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async deleteBook(id: string): Promise<Book | string> {
+    try {
+      const result = this.client.send({ cmd: 'deleteBook' }, id);
+
+      const value = await lastValueFrom(result);
+
+      return value;
+    } catch (error) {
+      return error;
+    }
+  }
 }
